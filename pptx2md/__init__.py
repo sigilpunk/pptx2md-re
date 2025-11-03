332ec4a80c1af36cb656329e99ba38be909b9dbf
sigilpunk <jskresl@gmail.com>
2025-11-03 13:34:38 -0500

    Updated `pptx2md\__init__.py`

--- pptx2md/__init__.py
+++ pptx2md/__init__.py
@@ -1,4 +1,3 @@
-from .initgen import extract_exports, generate_init, walk_and_generate
-from .main import generate_pptxt, parse_pptxt, parse_json_table, pptxt_to_md, main
+from .main import generate_pptxt, parse_pptxt, parse_json_table, pptxt_to_md, process_pptx
 
-__all__ = ['extract_exports', 'generate_init', 'walk_and_generate', 'generate_pptxt', 'parse_pptxt', 'parse_json_table', 'pptxt_to_md', 'main']
+__all__ = ['generate_pptxt', 'parse_pptxt', 'parse_json_table', 'pptxt_to_md', 'process_pptx']
